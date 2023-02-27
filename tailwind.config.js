77d1d33404aa80786f1e18005961fe3f7c41fa98
Toshiki Matsuoka <ruirairin@outlook.jp>
2023-02-27 22:23:42 +0900

    setting

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  options: {
+    // https://purgecss.com/safelisting.html#patterns
+    safelist: {
+      standard: [/^bg-/, /^text-/],
+    },
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
